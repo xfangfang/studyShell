d798dc4a8ec3d36c9be031dc4454006b6bc49541
xfang <2553041586@qq.com>
2016-09-21 00:08:34 +0800

    修复在mac上修改wifi的mac时响应延时的错误

--- changeMAC.sh
+++ changeMAC.sh
@@ -7,16 +7,20 @@ if [[ u -ne 0 ]]; then
 else
   if [[ $argv = 2 ]]; then
     ifconfig $1 ether $2
+    ifconfig $1 down
+    ifconfig $1 up
     echo $1 "has changed it's mac address to "$2
   elif [[ $argv = 1 ]]; then
     mac=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
     ifconfig $1 ether $mac
+    ifconfig $1 down
+    ifconfig $1 up
     echo $1 "has changed it's mac address to "$mac
   else
-    echo  "camc <interface> <mac>"
+    echo  "sudo camc <interface> <mac>"
     echo  "imput such like this"
     echo  "cmac en0 aa:bb:cc:dd:ee:ff"
     echo  "or"
-    echo  "cmmc en0"
+    echo  "sudo cmac en0"
   fi
 fi
