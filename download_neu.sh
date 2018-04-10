8aab5c4d8de35f7f8e752722627ec835ca810818
xfang <2553041586@qq.com>
2018-04-10 13:00:18 +0800

    不在下载脚本时修改密码

--- download_neu.sh
+++ download_neu.sh
@@ -1,11 +1,5 @@
-if [[ $1 = "" ]]; then
-    echo 请在命令后输入账号和密码 用空格分开
-elif [[ $2 = "" ]]; then
-    echo 请在命令后输入账号和密码 用空格分开
-else
-    curl https://raw.githubusercontent.com/xfangfang/studyShell/master/neuNet.sh -o ./neu --progress
-    sed -i '' '3s/test/'$1'/' ./neu
-    sed -i '' '4s/test/'$2'/' ./neu
-    chmod u+x ./neu
-    mv ./neu /usr/local/bin/neu
-fi
+
+curl https://raw.githubusercontent.com/xfangfang/studyShell/master/neuNet.sh -o ./neu --progress
+chmod u+x ./neu
+mv ./neu /usr/local/bin/neu
+
