b6d9b61ee16cba47348ddea6b3973c5202bd4570
xfang <2553041586@qq.com>
2018-04-10 10:05:35 +0800

    删除会导致错误的括号

--- download_neu.sh
+++ download_neu.sh
@@ -1,7 +1,7 @@
 if [[ $1 = "" ]]; then
-    echo 请在命令后输入账号和密码(用空格分开)
+    echo 请在命令后输入账号和密码 用空格分开
 elif [[ $2 = "" ]]; then
-    echo 请在命令后输入账号和密码(用空格分开)
+    echo 请在命令后输入账号和密码 用空格分开
 else
     curl https://raw.githubusercontent.com/xfangfang/studyShell/master/neuNet.sh -o ./neu --progress
     sed -i '' '3s/test/'$1'/' ./neu
